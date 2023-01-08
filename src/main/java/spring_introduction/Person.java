e640c0393fb9f93c7af6b4799643e9b8b3cd5f46
KselSy <branhert.anton@mail.ru>
2023-01-08 16:14:28 +0300

    DI with setter

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,10 +3,19 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
+
     public void callYourPet(){
         System.out.println("Hello, my lovely Pet!");
         pet.say();
