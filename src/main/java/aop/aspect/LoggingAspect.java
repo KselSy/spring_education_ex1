ad5eed960f6225c07fd0a3381f1980b9540b947d
KselSy <branhert.anton@mail.ru>
2023-01-09 13:25:32 +0300

    Знакомство с AOP

---
+++ src/main/java/aop/aspect/LoggingAspect.java
@@ -0,0 +1,20 @@
+package aop.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice(){
+        System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
+    @After("execution(public void getBook())")
+    public void afterGetBookAdvice(){
+        System.out.println("afterGetBookAdvice: книга взята");
+    }
+}
