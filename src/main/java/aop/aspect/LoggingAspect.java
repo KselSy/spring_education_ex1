d8acb0ea84ece96038090b0229ddce48f802e7ab
KselSy <branhert.anton@mail.ru>
2023-01-09 13:49:43 +0300

    Pointcut

--- src/main/java/aop/aspect/LoggingAspect.java
+++ src/main/java/aop/aspect/LoggingAspect.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
-    @Before("execution(public void getBook())")
+    @Before("execution(public void get*())")
     public void beforeGetBookAdvice(){
         System.out.println("beforeGetBookAdvice: попытка получить книгу");
     }
-    @After("execution(public void getBook())")
+    @Before("execution(* returnBook())")
+    public void beforeReturnBookAdvice(){
+        System.out.println("beforeGetBookAdvice: попытка вернуть книгу");
+    }
+    @After("execution(public void aop.SchoolLibrary.getBook())")
     public void afterGetBookAdvice(){
         System.out.println("afterGetBookAdvice: книга взята");
     }
